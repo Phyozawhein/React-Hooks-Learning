6316b4dfaa5207b1d243d565629c30aaed90d805
PhyoZawHein <phyozawhein1997@gmail.com>
2021-01-28 18:16:51 -0500

    Finished the React Hooks section

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,78 @@
-import React from 'react';
-
+import React, {useReducer , useCallback, useMemo, useEffect } from 'react';
+import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
+import ErroModal from '../UI/ErrorModal';
 import Search from './Search';
+import useHttp from '../../hooks/http';
+
+const ingredientsReducer = (state, action)=>{
+  switch(action.type){
+    case 'SET' : return action.ingredients;
+    case 'ADD' : return [ ...state, action.ingredient];
+    case 'DELETE' : return [...state.filter(ing => ing.id !== action.id)];
+    default: throw new Error('Should not get there ! '); 
+  }
+}
+
+
 
 function Ingredients() {
+
+  const [ingredients, dispatch] = useReducer(ingredientsReducer, []);
+  
+  const { isLoading, data, error, reqExtra,  sendRequest, reqIdentifier, clear} = useHttp();
+
+  useEffect(()=>{
+    if(!isLoading && !error && reqIdentifier === 'REMOVE_INGREDIENT'){
+      dispatch({type: 'DELETE', id: reqExtra})
+    }
+    else if(!isLoading && !error & reqIdentifier === 'ADD_INGREDIENT'){
+      dispatch({
+        type: 'ADD',
+        ingredient: {id: data.name, ...reqExtra}
+      })
+    }
+
+  },[data, reqExtra,reqIdentifier, isLoading, error]);
+
+  const filterIngredients = useCallback( filterIngs =>{
+
+   dispatch({type:'SET', ingredients: filterIngs})
+  },[]);
+
+
+
+
+  const addIngredientHandler = useCallback(ingredient =>{
+
+    sendRequest('https://react-hooks-6c9e3-default-rtdb.firebaseio.com/ingredients.json', 
+    'POST', 
+    JSON.stringify(ingredient), 
+    ingredient,
+    'ADD_INGREDIENT');
+
+},[sendRequest])
+
+  const removeIngridentHandler = useCallback(id =>{
+   sendRequest(`https://react-hooks-6c9e3-default-rtdb.firebaseio.com/${id}.json`,
+   'DELETE',
+    null,
+    id,
+    'REMOVE_INGREDIENT'
+    )
+
+  },[sendRequest])
+  const ingredientList = useMemo(()=>{
+    return <IngredientList ingredients ={ingredients} onRemoveItem={removeIngridentHandler}/>
+  },[ingredients, removeIngridentHandler]);
+
   return (
     <div className="App">
-      <IngredientForm />
-
+      {error ? <ErroModal onClose={clear}> {error}</ErroModal> : null}
+      <IngredientForm onAddIngredient ={addIngredientHandler} loading ={isLoading} />
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadIngredients={filterIngredients}/>
+        {ingredientList}
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,50 @@
-import React from 'react';
-
+import React, { useState, useEffect, useRef } from 'react';
+import useHttp from '../../hooks/http';
+import ErrorModal from '../UI/ErrorModal';
 import Card from '../UI/Card';
+
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadIngredients } = props ;
+  const [search, setSearch] = useState('');
+  const inputRef =useRef();
+  const {isLoading,data,error,sendRequest, clear } = useHttp();
+  useEffect(()=>{
+      const timer=  setTimeout(()=>{
+      if(search === inputRef.current.value){
+        const query = search.length === 0 ? '' : `?orderBy="title"&equalTo="${search}"` ;
+        sendRequest('https://react-hooks-6c9e3-default-rtdb.firebaseio.com/ingredients.json' + query, 'GET');       
+      }
+    },500)
+    return ()=> { 
+        clearTimeout(timer);
+    };
+  },[search, onLoadIngredients, inputRef,sendRequest]);
+
+  useEffect(()=>{
+    if(!isLoading && error && data){
+      const loadedIngredients =[];
+      for( const key in data){
+        loadedIngredients.push({
+          id: key,
+          title : data[key].title,
+          amount: data[key].amount
+        });
+      }
+      onLoadIngredients(loadedIngredients)
+    }
+  },[data,isLoading,error])
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          {isLoading && <span>Loading...</span>}
+          <input
+           ref={inputRef}
+           type="text"  value={search} onChange={event=> setSearch(event.target.value)}/>
         </div>
       </Card>
     </section>
